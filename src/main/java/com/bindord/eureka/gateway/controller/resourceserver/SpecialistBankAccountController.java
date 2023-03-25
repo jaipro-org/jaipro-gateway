67b385fae944c58489e8d4ed87b8495efbfc231e
Petter Carranza <peterpaul.0194@gmail.com>
2023-03-24 19:24:20 -0500

    adding new controller for specialist bank account

---
+++ src/main/java/com/bindord/eureka/gateway/controller/resourceserver/SpecialistBankAccountController.java
@@ -0,0 +1,78 @@
+package com.bindord.eureka.gateway.controller.resourceserver;
+
+import com.bindord.eureka.gateway.wsc.ResourceServerClientConfiguration;
+import com.bindord.eureka.resourceserver.model.SpecialistBankAccount;
+import com.bindord.eureka.resourceserver.model.SpecialistBankAccountDto;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("${service.ingress.context-path}/specialist-bank-account")
+@Slf4j
+public class SpecialistBankAccountController {
+
+    private final ResourceServerClientConfiguration resourceServerClientConfiguration;
+
+    @ApiResponse(description = "Persist a Specialist's bank account",
+            responseCode = "200")
+    @PostMapping(value = "",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<SpecialistBankAccount> persistSpecialistBankAccount(@Valid @RequestBody SpecialistBankAccountDto SpecialistBankAccount) {
+        return resourceServerClientConfiguration.init()
+                .post()
+                .uri("/specialist-bank-account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(SpecialistBankAccount), SpecialistBankAccountDto.class)
+                .retrieve()
+                .bodyToMono(SpecialistBankAccount.class)
+                .subscribeOn(Schedulers.parallel());
+    }
+
+    @ApiResponse(description = "Update Specialist's bank account",
+            responseCode = "200")
+    @PutMapping(value = "",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<SpecialistBankAccount> updateSpecialistBankAccount(@Valid @RequestBody SpecialistBankAccountDto SpecialistBankAccount) {
+        return resourceServerClientConfiguration.init()
+                .put()
+                .uri("/specialist-bank-account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(SpecialistBankAccount), SpecialistBankAccountDto.class)
+                .retrieve()
+                .bodyToMono(SpecialistBankAccount.class)
+                .subscribeOn(Schedulers.parallel());
+    }
+
+    @ApiResponse(description = "Get all Specialist's bank account By SpecialistId",
+            responseCode = "200")
+    @GetMapping(value = "/specialist/{specialistId}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Flux<SpecialistBankAccount> findAllBySpecialistId(@PathVariable UUID specialistId) {
+        return resourceServerClientConfiguration.init()
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/specialist-bank-account/list/{id}")
+                        .build(specialistId))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(SpecialistBankAccount.class);
+    }
+}
