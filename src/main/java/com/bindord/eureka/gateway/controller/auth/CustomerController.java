971ec85b4e8acdafa2a51ccdeb12108c007dee3c
Petter Carranza <peterpaul.0194@gmail.com>
2023-04-11 00:02:25 -0500

    HEP-143 HEP-149 get customer full information

--- src/main/java/com/bindord/eureka/gateway/controller/auth/CustomerController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/auth/CustomerController.java
@@ -3,6 +3,8 @@ package com.bindord.eureka.gateway.controller.auth;
 import com.bindord.eureka.auth.model.Customer;
 import com.bindord.eureka.auth.model.CustomerPersist;
 import com.bindord.eureka.gateway.wsc.AuthClientConfiguration;
+import com.bindord.eureka.gateway.wsc.ResourceServerClientConfiguration;
+import com.bindord.eureka.resourceserver.model.CustomerInformationDto;
 import com.bindord.eureka.resourceserver.model.CustomerInformationUpdateDto;
 import com.bindord.eureka.resourceserver.model.CustomerLocationUpdateDto;
 import com.bindord.eureka.resourceserver.model.CustomerPasswordUpdateDto;
@@ -10,6 +12,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -27,6 +32,7 @@ import javax.validation.Valid;
 public class CustomerController {
 
     private final AuthClientConfiguration authClientConfiguration;
+    private final ResourceServerClientConfiguration resourceServerClientConfiguration;
 
     @ApiResponse(description = "Persist a customer",
             responseCode = "200")
@@ -94,5 +100,19 @@ public class CustomerController {
                 .retrieve()
                 .bodyToMono(Boolean.class)
                 .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @ApiResponse(description = "Get customer full information",
+            responseCode = "200")
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<CustomerInformationDto> getCustomerInformation(@PathVariable UUID id) {
+        return resourceServerClientConfiguration.init()
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/customer/{id}/information")
+                        .build(id))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(CustomerInformationDto.class);
     }
 }
