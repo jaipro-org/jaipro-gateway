8fa3ad1e714b1e634a09c5d58d337c65b97b012d
Petter Carranza <peterpaul.0194@gmail.com>
2023-06-11 22:50:40 -0500

    HEP-46 HEP-47 notification controller fix

--- src/main/java/com/bindord/eureka/gateway/controller/resourceserver/NotificationController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/resourceserver/NotificationController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +63,7 @@ public class NotificationController {
 
     @ApiResponse(description = "Update notification",
             responseCode = "200")
-    @PostMapping(value = "",
+    @PutMapping(value = "",
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public Mono<Notification> update(@RequestBody NotificationUpdateDto notification) {
         return resourceServerClientConfiguration.init()
