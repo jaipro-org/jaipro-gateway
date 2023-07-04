efae3aeb0166ff2862befb247ad8c3c564a1389a
Panda <63072537+giovanny110@users.noreply.github.com>
2023-07-03 22:49:40 -0500

    HEP-92: implement API list projects

--- src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ServiceRequestController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ServiceRequestController.java
@@ -1,9 +1,24 @@
 package com.bindord.eureka.gateway.controller.resourceserver;
 
+import com.bindord.eureka.gateway.domain.specialist.SpecialistBiographyDto;
+import com.bindord.eureka.gateway.wsc.ResourceServerClientConfiguration;
+import com.bindord.eureka.resourceserver.model.BasePaginateResponseServiceRequestListDto;
+import com.bindord.eureka.resourceserver.model.BasePaginateResponseSpecialistResultSearchDTO;
+import com.bindord.eureka.resourceserver.model.BaseSearch;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import javax.validation.Valid;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -11,5 +26,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ServiceRequestController {
 
+    private final ResourceServerClientConfiguration resourceServerClientConfiguration;
 
+    @ApiResponse(description = "Get list services request by customer id", responseCode = "200")
+    @GetMapping(value = "/list/{customerId}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<BasePaginateResponseServiceRequestListDto> listByCustomerId(@PathVariable UUID customerId, @Valid BaseSearch searchDto) {
+        return resourceServerClientConfiguration.init()
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/service-request/list/{customerId}")
+                        .queryParam("pageNumber", searchDto.getPageNumber())
+                        .queryParam("pageSize", searchDto.getPageSize())
+                        .build(customerId))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(BasePaginateResponseServiceRequestListDto.class)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
 }
