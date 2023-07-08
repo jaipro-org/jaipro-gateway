7e982c76b0a6d02b26d52260ca2df8b8bdd531b1
Panda <63072537+giovanny110@users.noreply.github.com>
2023-07-07 20:17:20 -0500

    HEP-89: integration API create ServiceRequest

--- src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ServiceRequestController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ServiceRequestController.java
@@ -1,23 +1,26 @@
 package com.bindord.eureka.gateway.controller.resourceserver;
 
-import com.bindord.eureka.gateway.domain.specialist.SpecialistBiographyDto;
+import com.bindord.eureka.gateway.domain.service.ServiceRequestDto;
+import com.bindord.eureka.gateway.services.ServiceRequestService;
 import com.bindord.eureka.gateway.wsc.ResourceServerClientConfiguration;
 import com.bindord.eureka.resourceserver.model.BasePaginateResponseServiceRequestListDto;
-import com.bindord.eureka.resourceserver.model.BasePaginateResponseSpecialistResultSearchDTO;
 import com.bindord.eureka.resourceserver.model.BaseSearch;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -27,6 +30,16 @@ import java.util.UUID;
 public class ServiceRequestController {
 
     private final ResourceServerClientConfiguration resourceServerClientConfiguration;
+    private final ServiceRequestService service;
+
+    @ApiResponse(description = "create services request by customer id", responseCode = "200")
+    @PostMapping(value = "",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public Mono<Void> create(@RequestPart List<FilePart> images, @RequestPart @Valid ServiceRequestDto createDto){
+        return service.create(images, createDto);
+    }
 
     @ApiResponse(description = "Get list services request by customer id", responseCode = "200")
     @GetMapping(value = "/list/{customerId}", produces = {MediaType.APPLICATION_JSON_VALUE})
--- src/main/java/com/bindord/eureka/gateway/domain/service/ServiceRequestDto.java
+++ src/main/java/com/bindord/eureka/gateway/domain/service/ServiceRequestDto.java
@@ -1,7 +1,12 @@
-package com.bindord.eureka.gateway.domain;
+package com.bindord.eureka.gateway.domain.service;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Setter
+@Getter
 public class ServiceRequestDto {
 
     private Integer professionId;
